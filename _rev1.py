1520ca507df3ff56cf7b55b5fd0bddaba4266b86
RudreshJayaram <skrvmay30@gmail.com>
2016-03-13 15:03:31 +0530

    god

--- _rev1.py
+++ _rev1.py
@@ -11,7 +11,7 @@ os.system("./servod")
 os.chdir("/root/")
 
 
-#os.system("git clone https://github.com/RudreshJayaram/revolution.git")
+os.system("git clone https://github.com/RudreshJayaram/revolution.git")
 os.chdir("revolution")
 os.system("cp /root/PiBits/ServoBlaster/user/mailbox.c mailbox.c")
 os.system("cp /root/PiBits/ServoBlaster/user/mailbox.h mailbox.h")
@@ -20,16 +20,17 @@ os.system("make servod")
 os.system("./servod")
 #************************** This is the uart mode set up *********************************************
 os.system("mv /boot/cmdline.txt /boot/cmdline.txt.bkp")
-os.system("mv cmdline.txt /boot/cmdline.txt")
-os.system("mv inittab /etc/inittab")
+os.system("cp cmdline.txt.grp /boot/cmdline.txt")
+os.system("mv /etc/inittab /etc/inittab.bkp")
+os.system("cp inittab.grp /etc/inittab")
 
 #************************** This is the apn mode set up *********************************************
 os.system("sudo apt-get install hostapd isc-dhcp-server")
 os.system("mv  /etc/dhcp/dhcpd.conf  /etc/dhcp/dhcpd.conf.bkp")
-os.system("mv dhcpd.conf  /etc/dhcp/dhcpd.conf")
+os.system("cp dhcpd.conf.grp  /etc/dhcp/dhcpd.conf")
 
 os.system("mv /etc/default/isc-dhcp-server /etc/default/isc-dhcp-server.bkp")
-os.system("mv isc-dhcp-server /etc/default/isc-dhcp-server")
+os.system("cp isc-dhcp-server.grp /etc/default/isc-dhcp-server")
 
 os.system("sudo ifdown wlan0")
 
@@ -41,15 +42,15 @@ os.system("cp /etc/network/interfaces.apnmode /etc/network/interfaces")
 os.system("sudo ifconfig wlan0 1.2.3.4")
 
 
-#os.system("mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bkp") this file has some issues to deal with
-os.system("mv hostapd.conf /etc/hostapd/hostapd.conf")
+os.system("mv /etc/hostapd/hostapd.conf /etc/hostapd/hostapd.conf.bkp") 
+os.system("cp hostapd.conf.grp /etc/hostapd/hostapd.conf")
 
 
 os.system("mv /etc/default/hostapd /etc/default/hostapd.bkp")
-os.system("mv hostapd /etc/default/hostapd")
+os.system("cp hostapd.grp /etc/default/hostapd")
 
 os.system("mv /etc/sysctl.conf /etc/sysctl.conf.bkp")
-os.system("mv sysctl.conf /etc/sysctl.conf")
+os.system("cp sysctl.conf.grp /etc/sysctl.conf")
 os.system("sudo sh -c \"echo 1 > /proc/sys/net/ipv4/ip_forward\"")
 
 
@@ -75,15 +76,22 @@ os.system("sudo service isc-dhcp-server status")
 os.system("sudo update-rc.d hostapd enable")
 os.system("sudo update-rc.d isc-dhcp-server enable")
 os.system("sudo mv /usr/share/dbus-1/system-services/fi.epitest.hostap.WPASupplicant.service ~/")
+
 os.system("mv /etc/rc.local /etc/rc.local.bkp")
-os.system("mv rc.local /etc/rc.local")
+os.system("cp rc.local.grp /etc/rc.local")
 os.system( "chmod 777 /etc/rc.local")
 
 os.system("mv bt.py /root/")
+os.chdir("/root/revolution")
+os.system("mv  /root/revolution/.hidden /root/.hidden")
 
+os.system("cp  /etc/bash.bashrc /etc/bash.bashrc.bkp")
+file = open("/etc/bash.bashrc","a")
+file.write( "alias ls=\"export GLOBIGNORE_TMP=$GLOBIGNORE; export GLOBIGNORE=$GLOBIGNORE:.*:$(tr \'\\n\' \':\' < /root/.hidden); ls -dC * ;export GLOBIGNORE=$GLOBIGNORE_TMP; export GLOBIGNORE_TMP=\'\'\"")
+file.close()
 os.system("clear")
 time.sleep(2)
+
 print "all done"
 os.system("sudo reboot")
-
 
