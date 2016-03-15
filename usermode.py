b9ac0193cd367626492306d2b4bea1b1ee0048f9
RudreshJayaram <skrvmay30@gmail.com>
2016-03-15 16:38:28 +0530

    r

--- usermode.py
+++ usermode.py
@@ -7,6 +7,6 @@ os.system("mv /etc/network/interfaces.bkp /etc/network/interfaces")
 os.system("mv /etc/hostapd/hostapd.conf.bkp /etc/hostapd/hostapd.conf") 
 os.system("mv /etc/default/hostapd.bkp /etc/default/hostapd")
 os.system("mv /etc/sysctl.conf.bkp /etc/sysctl.conf")
-os.system("mv  /etc/bash.bashrc.bkp /etc/bash.bashrc")
+#os.system("mv  /etc/bash.bashrc.bkp /etc/bash.bashrc")
 os.system("sudo reboot")
 
