2967a176969b36e16c14dce465b5388b250b3440
RudreshJayaram <skrvmay30@gmail.com>
2016-03-15 15:31:13 +0530

    g

--- usermode.py
+++ usermode.py
@@ -1,15 +1,11 @@
 import time
 import os
-os.system("mv /boot/cmdline.txt.bkp /boot/cmdline.txt")
-os.system("mv /etc/inittab.bkp /etc/inittab")
 os.system("mv  /etc/dhcp/dhcpd.conf.bkp  /etc/dhcp/dhcpd.conf")
 os.system("mv /etc/default/isc-dhcp-server.bkp /etc/default/isc-dhcp-server")
 os.system("mv /etc/network/interfaces.bkp /etc/network/interfaces")
 os.system("mv /etc/hostapd/hostapd.conf.bkp /etc/hostapd/hostapd.conf") 
 os.system("mv /etc/default/hostapd.bkp /etc/default/hostapd")
 os.system("mv /etc/sysctl.conf.bkp /etc/sysctl.conf")
-os.system("mv /etc/rc.local.bkp /etc/rc.local")
-os.system( "chmod 777 /etc/rc.local")
 os.system("mv  /etc/bash.bashrc.bkp /etc/bash.bashrc")
 os.system("sudo reboot")
 
